a9722b23c6bf0e7fedba5c17c66049cc198ab4a8
WORLDCRACKER <37765848+WORLDCRACKER@users.noreply.github.com>
2018-03-25 09:31:00 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,9 @@
+<html><head>kaka workshop</head>
+<body>
+<form action ="post.php" method="post">
+Full Name :<input type="text" name ="user"><br/>
+Select Your Country:<input type ="text" name="Dalka"><br/>
+<input type ="submit" value="Register"/>
+</form>
+</body>
+</html>
